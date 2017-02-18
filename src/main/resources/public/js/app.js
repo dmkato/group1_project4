a7bd619aa6de3e8f95acfe8bd900117c6ce86f1d
dmkato <dmkato@gmail.com>
2017-02-17 23:22:51 -0800

    Fixed little morty size

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -189,38 +189,52 @@ function displayShip(ship){
 
 /* Set up Game */
 function chooseShips(){
-  //make #MyBoard touchable
-  $('#MyBoard').on("click", "td", function() {
+  //Make #shipStatus touchable
+  $('#shipStatus').on("click", "tr", function() {
 
     // // Display Coords in footer
-    var coords = $(this).attr('id').split("_"); //works. Assigns coords to correct coordinates. Send to placeShip
 
     // Fire or scan Coord
     if(didPressPlaceShip){
-      $('footer #status').text("Choose Start Coordinate On Small Board! Choose block to left for left orientation, above for vertical orientation, etc.");
+        var ship = $(this).attr('id');      //works. Assigns ship1, ship2, ... ship5 to var ship
+        $('footer #status').text(ship + " Choose Start Coordinate On Small Board! Choose block to left for left orientation, above for vertical orientation, etc.");
     } else {
         $('footer #status').text("That is not right.");
-
     }
-    var orientation = didPressRotate;
-
-    placeShip(ship, coords[0], coords[1], orientation);
-
-    // Make grid touchable to fire at
-    $('#TheirBoard').on("click", "td", function() {
+    //make #MyBoard touchable
+    $('#MyBoard').on("click", "td", function() {
 
       // // Display Coords in footer
-      var coords = $(this).attr('id').split("_");
+      var coords = $(this).attr('id').split("_"); //works. Assigns coords to correct coordinates. Send to placeShip
 
       // Fire or scan Coord
-      if(didPressScan){
-        scan(coords[0], coords[1]);
-          $('footer #status').text("Scaned " + coords[0] + ", " + coords[1]);
+      if(didPressPlaceShip){
+        $('footer #status').text("Choose Start Coordinate On Small Board! Choose block to left for left orientation, above for vertical orientation, etc.");
       } else {
-        fire(coords[0], coords[1]);
+          $('footer #status').text("That is not right.");
+
       }
+      var orientation = didPressRotate;
+
+      placeShip(ship, coords[0], coords[1], orientation);
+
+      // Make grid touchable to fire at
+      $('#TheirBoard').on("click", "td", function() {
+
+        // // Display Coords in footer
+        var coords = $(this).attr('id').split("_");
+
+        // Fire or scan Coord
+        if(didPressScan){
+          scan(coords[0], coords[1]);
+            $('footer #status').text("Scaned " + coords[0] + ", " + coords[1]);
+        } else {
+          fire(coords[0], coords[1]);
+        }
+      });
     });
   });
+
 }
 
 /* Creates grid of 10 squares for MyBoard and TheirBoard */
